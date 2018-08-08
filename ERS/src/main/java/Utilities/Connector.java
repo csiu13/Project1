86e2cd22a768513d4b0a713124ba98d8f6056c6f
Christopher <chrisissiu@gmail.com>
2018-08-08 13:00:56 -0400

    bad things

--- ERS/src/main/java/Utilities/Connector.java
+++ ERS/src/main/java/Utilities/Connector.java
@@ -10,34 +10,34 @@ import java.util.Properties;
 
 public class Connector {
     public static Connection getConnection() {
-		InputStream in = null;
+		//InputStream in = null;
 		
 		try {
-			Properties props = new Properties();
-			//System.out.println(System.getProperty("user.dir"));
-			in  = new FileInputStream("ERS\\src\\main\\resources\\connections.properties");
-			props.load(in);
+			// Properties props = new Properties();
+			// //System.out.println(System.getProperty("user.dir"));
+			// in  = new FileInputStream("ERS\\src\\main\\resources\\connections.properties");
+			// props.load(in);
 			
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
 			Connection conn = null;
-			String endpoint = props.getProperty("jdbc.url");
-			//System.out.println(endpoint);
-			String username = props.getProperty("jdbc.username");
-			String password = props.getProperty("jdbc.password");
+			// String endpoint = props.getProperty("jdbc.url");
+			// //System.out.println(endpoint);
+			// String username = props.getProperty("jdbc.username");
+			// String password = props.getProperty("jdbc.password");
 			
-			conn = DriverManager.getConnection(endpoint, username, password);
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@siudb.ckqj1sf1jfmd.us-east-1.rds.amazonaws.com:1521:ORCL", "christopher", "password145");
 
 			return conn;
 		} catch(Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		} //finally {
+		// 	try {
+		// 		in.close();
+		// 	} catch (IOException e) {
+		// 		e.printStackTrace();
+		// 	}
+		// }
 		
 		return null;
 	}
